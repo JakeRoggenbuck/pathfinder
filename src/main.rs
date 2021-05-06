8c6ddc47f39324ce974010920a2a2d5375e23430
jakeroggenbuck <jakeroggenbuck2@gmail.com>
2021-05-06 10:46:30 -0700

    Add Path and Finder

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,39 @@
 use std::env;
 
-fn split_path(path: &String) -> Vec<&str> {
-    let split = path.split(":");
-    let vec: Vec<&str> = split.collect();
-    return vec;
+trait Finder {
+    fn split_path(&self) -> Vec<&str>;
+    fn list(&self);
 }
+
+struct Path {
+    path: String,
+    places: Vec<&'static str>
+}
+
+impl Finder for Path {
+    fn split_path(&self) -> Vec<&str> {
+        let split = self.path.split(":");
+        let vec: Vec<&str> = split.collect();
+        return vec;
+    }
+    fn list(&self) {
+        for place in &self.places {
+            println!("{}", place);
+        }
+    }
+}
+
 
 fn main() {
     let path = env::var("PATH");
     match path {
         Ok(path) => {
-            let vec = split_path(&path);
-            println!("{:?}", vec);
+            let finder = Path {
+                path: path,
+                places: Vec::new(),
+            };
+            finder.split_path();
+            finder.list();
         }
         Err(e) => eprint!("{}", e),
     }
