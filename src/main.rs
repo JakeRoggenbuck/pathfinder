bef18810fcdbe2c53a5de698d60d93b02c05e8e4
jakeroggenbuck <jakeroggenbuck2@gmail.com>
2021-05-07 00:16:02 -0700

    Add find commands

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 use std::env;
+use std::process;
 
 trait Finder {
     fn split_path(&mut self);
-    fn list(&self);
+    fn list(&self, locations: Option<Vec<u8>>);
+    fn find_locations(&self, word: String) -> Vec<u8>;
 }
 
 struct Path {
@@ -21,14 +23,40 @@ impl Finder for Path {
         }
         self.places = vec;
     }
-    fn list(&self) {
-        for place in &self.places {
-            println!("{}", place);
+    fn list(&self, locations: Option<Vec<u8>>) {
+        match locations {
+            Some(l) => {
+                let mut index: u8 = 0;
+                for place in &self.places {
+                    if l.contains(&index) {
+                        println!("{}", place);
+                    }
+                    index += 1;
+                }
+            }
+            None => {
+                for place in &self.places {
+                    println!("{}", place);
+                }
+            }
         }
+    }
+    fn find_locations(&self, word: String) -> Vec<u8> {
+        let mut locations = Vec::<u8>::new();
+        let mut index: u8 = 0;
+        for place in &self.places {
+            if place.contains(&word) {
+                locations.push(index);
+            }
+            index += 1;
+        }
+        return locations;
     }
 }
 
 fn main() {
+    let args: Vec<String> = env::args().collect();
+
     let path = env::var("PATH");
     match path {
         Ok(path) => {
@@ -37,7 +65,21 @@ fn main() {
                 places: Vec::new(),
             };
             finder.split_path();
-            finder.list();
+
+            if args.len() >= 1 {
+                match args[1].as_ref() {
+                    "version" | "v" => println!("Version: 0.1"),
+                    "find" | "f" => {
+                        if args.len() >= 3 {
+                            let loc = finder.find_locations(args[2].to_string());
+                            finder.list(Some(loc));
+                        } else {
+                            finder.list(None);
+                        }
+                    }
+                    _ => process::exit(0),
+                };
+            }
         }
         Err(e) => eprint!("{}", e),
     }
