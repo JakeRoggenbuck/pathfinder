2f55e4692097461c3b7fdfcf26f81c193a943130
JakeRoggenbuck <35516367+JakeRoggenbuck@users.noreply.github.com>
2021-05-07 19:40:38 -0700

    Make add command (#1)
    * Create basic add feature
    
    * Finish add command
    
    * Add docs
    
    * Add command to readme

--- README.md
+++ README.md
@@ -8,3 +8,4 @@ A simple utility to view and search your $PATH
 | `v`       | `--version` | Display the version and exit                               |
 | `l`       | `--list`    | List all locations in $PATH (same as find with no keyword) |
 | `f`       | `--find`    | Find locations in $PATH including search keyword           |
+| `a`       | `--add`     | Add location to the $PATH, and open a new bash prompt      |
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,21 @@
 use std::env;
+use std::path::Path;
 use std::process;
+use std::process::Command;
 
-trait Finder {
+trait PathFinder {
     fn split_path(&mut self);
     fn list(&self, locations: Option<Vec<u8>>);
     fn find_locations(&self, word: String) -> Vec<u8>;
+    fn add(&self, location: String);
 }
 
-struct Path {
+struct Finder {
     path: String,
     places: Vec<String>,
 }
 
-impl Finder for Path {
+impl PathFinder for Finder {
     fn split_path(&mut self) {
         let split = self.path.split(":");
         let mut vec = Vec::<String>::new();
@@ -52,6 +55,29 @@ impl Finder for Path {
         }
         return locations;
     }
+    fn add(&self, location: String) {
+        println!("Adding {} to $PATH", location);
+        // Checks if the file location exists
+        if Path::new(&location).exists() {
+            // Set the environmental variable, same as PATH=location:$PATH
+            env::set_var(
+                "PATH",
+                format!("{}:{}", location, env::var("PATH").unwrap()),
+            );
+
+            // Open a new instance of bash on top of the program
+            Command::new("bash")
+                .spawn()
+                .expect("Failed to execute command");
+
+            // Wait for a long time to let the new bash prompt exist a while
+            let mut child = Command::new("sleep").arg("infinity").spawn().unwrap();
+            let _result = child.wait().unwrap();
+
+        } else {
+            eprintln!("The location {}, does not exist", location);
+        }
+    }
 }
 
 fn usage() {
@@ -64,6 +90,7 @@ fn usage() {
         v, --version    Display the version and exit
         l, --list       List all locations in $PATH (same as find with no keyword)
         f, --find       Find locations in $PATH including search keyword
+        a, --add        Add location to the $PATH, and open a new bash prompt
         "
     );
     process::exit(0);
@@ -74,11 +101,16 @@ fn version() {
     process::exit(0);
 }
 
-fn arg_parser(args: Vec<String>, finder: Path) {
+fn arg_parser(args: Vec<String>, finder: Finder) {
     if args.len() >= 1 {
         match args[1].as_ref() {
             "--version" | "-v" | "v" => version(),
             "--help" | "-h" | "h" => usage(),
+            "--add" | "-a" | "a" => {
+                if args.len() >= 3 {
+                    finder.add(args[2].to_owned());
+                }
+            }
             "--find" | "-f" | "f" => {
                 if args.len() >= 3 {
                     let loc = finder.find_locations(args[2].to_string());
@@ -104,7 +136,7 @@ fn main() {
     let path = env::var("PATH");
     match path {
         Ok(path) => {
-            let mut finder = Path {
+            let mut finder = Finder {
                 path: path,
                 places: Vec::new(),
             };
