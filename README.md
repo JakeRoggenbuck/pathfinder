501f9b31a4a847e9b92845fec87ee5e720bbfaf0
jakeroggenbuck <jakeroggenbuck2@gmail.com>
2021-05-07 20:08:21 -0700

    Add number, and purge commands

--- README.md
+++ README.md
@@ -9,3 +9,5 @@ A simple utility to view and search your $PATH
 | `l`       | `--list`    | List all locations in $PATH (same as find with no keyword) |
 | `f`       | `--find`    | Find locations in $PATH including search keyword           |
 | `a`       | `--add`     | Add location to the $PATH, and open a new bash prompt      |
+| `p`       | `--purge`   | Unsets everything in $PATH, it will be empty               |
+| `n`       | `--number`  | List locations in $PATH with numbers on side               |
--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,11 @@ use std::process::Command;
 
 trait PathFinder {
     fn split_path(&mut self);
-    fn list(&self, locations: Option<Vec<u8>>);
+    fn list(&self, locations: Option<Vec<u8>>, enumerate: bool);
     fn find_locations(&self, word: String) -> Vec<u8>;
     fn spawn(&self);
     fn add(&self, location: String);
+    fn purge(&self);
 }
 
 struct Finder {
@@ -27,20 +28,30 @@ impl PathFinder for Finder {
         }
         self.places = vec;
     }
-    fn list(&self, locations: Option<Vec<u8>>) {
+    fn list(&self, locations: Option<Vec<u8>>, enumerate: bool) {
         match locations {
             Some(l) => {
                 let mut index: u8 = 0;
                 for place in &self.places {
                     if l.contains(&index) {
-                        println!("{}", place);
+                        if enumerate {
+                            println!("{}\t{}", index, place);
+                        } else {
+                            println!("{}", place);
+                        }
                     }
                     index += 1;
                 }
             }
             None => {
+                let mut index: u8 = 0;
                 for place in &self.places {
-                    println!("{}", place);
+                    if enumerate {
+                        println!("{}", place);
+                    } else {
+                        println!("{}\t{}", index, place);
+                    }
+                    index += 1;
                 }
             }
         }
@@ -58,7 +69,7 @@ impl PathFinder for Finder {
     }
     fn spawn(&self) {
         // Open a new instance of bash on top of the program
-        Command::new("bash")
+        Command::new("/bin/bash")
             .spawn()
             .expect("Failed to execute command");
 
@@ -81,6 +92,10 @@ impl PathFinder for Finder {
             eprintln!("The location {}, does not exist", location);
         }
     }
+    fn purge(&self) {
+        env::set_var("PATH", "UNSET");
+        self.spawn();
+    }
 }
 
 fn usage() {
@@ -94,6 +109,8 @@ fn usage() {
         l, --list       List all locations in $PATH (same as find with no keyword)
         f, --find       Find locations in $PATH including search keyword
         a, --add        Add location to the $PATH, and open a new bash prompt
+        p, --purge      Unsets everything in $PATH, it will be empty
+        n, --number     List locations in $PATH with numbers on side
         "
     );
     process::exit(0);
@@ -117,12 +134,21 @@ fn arg_parser(args: Vec<String>, finder: Finder) {
             "--find" | "-f" | "f" => {
                 if args.len() >= 3 {
                     let loc = finder.find_locations(args[2].to_string());
-                    finder.list(Some(loc));
+                    finder.list(Some(loc), false);
                 } else {
-                    finder.list(None);
+                    finder.list(None, false);
                 }
             }
-            "--list" | "l" => finder.list(None),
+            "--list" | "l" => finder.list(None, false),
+            "--purge" | "p" => finder.purge(),
+            "--number" | "-n" | "n" => {
+                if args.len() >= 3 {
+                    let loc = finder.find_locations(args[2].to_string());
+                    finder.list(Some(loc), false);
+                } else {
+                    finder.list(None, false);
+                }
+            }
             _ => process::exit(0),
         };
     }
